863d75f55cf2a6bf0407c43c734468835f442717
0x12md10 <abish102001@gmail.com>
2026-03-31 00:36:12 +0530

    implement a hoc for managing window states and add the first window -terminal

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-import Welcome from "./Welcome.jsx"
-import Navbar from "./Navbar.jsx"
-import Dock from "./Dock.jsx"
+import Welcome from "#components/Welcome.jsx"
+import Navbar from "#components/Navbar.jsx"
+import Dock from "#components/Dock.jsx"
+import WindowControls from "#components/WindowControls.jsx"
 
-export {Navbar , Welcome , Dock}
+export {Navbar , Welcome , Dock , WindowControls}
